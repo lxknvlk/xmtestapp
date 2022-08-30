f0a6531ba2dda85ee3f0c4590afd2d91b09b3e87
lxknvlk <lxknvlk@gmail.com>
2022-08-30 10:53:42 +0300

    first screen finished

--- app/src/main/java/com/example/xmtestapp/view/MainActivity.kt
+++ app/src/main/java/com/example/xmtestapp/view/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.xmtestapp.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.xmtestapp.R
+import com.google.android.material.progressindicator.LinearProgressIndicator
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnStart: Button
+    private lateinit var llRoot: LinearLayout
+    private lateinit var piLoader: LinearProgressIndicator
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -19,11 +28,49 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnStart = findViewById(R.id.btnStart)
+        llRoot = findViewById(R.id.llRoot)
+        piLoader = findViewById(R.id.piLoader)
 
         btnStart.setOnClickListener {
+            val intent = Intent(this, SurveyActivity::class.java)
+            startActivity(intent)
+        }
 
+        viewModel.loadingLiveData.observe(this) { isLoading ->
+            if (isLoading){
+                showLoading()
+            } else {
+                hideLoading()
+            }
+        }
+
+        viewModel.questionsLiveData.observe(this) { questionsList ->
+            if (questionsList != null) {
+                btnStart.isEnabled = true
+                showSuccess(getString(R.string.questions_loaded))
+            } else {
+                showError(getString(R.string.server_error))
+            }
         }
 
         viewModel.fetchQuestions()
+    }
+
+    private fun hideLoading() {
+        piLoader.visibility = View.INVISIBLE
+    }
+
+    private fun showLoading() {
+        piLoader.visibility = View.VISIBLE
+    }
+
+    private fun showError(msg: String) {
+        Snackbar.make(llRoot, msg, Snackbar.LENGTH_SHORT)
+            .setBackgroundTint(ContextCompat.getColor(this, R.color.error_red)).show()
+    }
+
+    private fun showSuccess(msg: String) {
+        Snackbar.make(llRoot, msg, Snackbar.LENGTH_SHORT)
+            .setBackgroundTint(ContextCompat.getColor(this, R.color.success_green)).show()
     }
 }
--- app/src/main/java/com/example/xmtestapp/view/MainViewModel.kt
+++ app/src/main/java/com/example/xmtestapp/view/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.example.xmtestapp.view
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.xmtestapp.data.api.entity.QuestionEntity
 import com.example.xmtestapp.logic.GetQuestionsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -14,12 +15,15 @@ class MainViewModel @Inject constructor(
     private val getQuestionsUseCase: GetQuestionsUseCase
 ) : ViewModel() {
 
-    val successLiveData = MutableLiveData<Any?>(false)
+    val loadingLiveData = MutableLiveData<Boolean>(false)
+    val questionsLiveData = MutableLiveData<List<QuestionEntity>?>()
 
     fun fetchQuestions() {
+        loadingLiveData.postValue(true)
         viewModelScope.launch(Dispatchers.IO) {
             val questions = getQuestionsUseCase.getQuestions()
-            successLiveData.postValue(questions)
+            loadingLiveData.postValue(false)
+            questionsLiveData.postValue(questions)
         }
     }
 }
