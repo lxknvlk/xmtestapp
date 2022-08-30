64531745d3c6974a6dd64ce279cf15076a50d6ce
lxknvlk <lxknvlk@gmail.com>
2022-08-30 09:40:48 +0300

    added screens

--- app/src/main/java/com/example/xmtestapp/MainActivity.kt
+++ app/src/main/java/com/example/xmtestapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.xmtestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnStart: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart = findViewById(R.id.btnStart)
+
+        btnStart.setOnClickListener {
+            
+        }
     }
 }
